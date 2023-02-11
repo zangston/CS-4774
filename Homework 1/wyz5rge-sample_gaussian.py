74741796f18dc692ca07dc192a7727cd38081d26
zangston <winston.y.zhang@gmail.com>
2023-02-10 21:14:02 -0500

    Update wyz5rge-sample_gaussian.py
    Generate samples and create scatterplots but I'm not seeing anything??

--- Homework 1/wyz5rge-sample_gaussian.py
+++ Homework 1/wyz5rge-sample_gaussian.py
@@ -9,10 +9,18 @@ import matplotlib.pyplot as plt
 # define identity matrix as specified in Case I according to assignment description
 matrix_identity = numpy.identity(2)
 
-# define diagronal matrix
-matrix_diag = numpy.diag([2, 4])
+# define diagonal matrix
+matrix_diagonal = numpy.diag([2, 4])
 
 # define number of samples taken
 sample_size = 500
 
+# generate samples from distributions using each covariance matrix
+samples_cm_identity = numpy.random.multivariate_normal([0, 0], matrix_identity, sample_size)
+samples_cm_diagonal = numpy.random.multivariate_normal([0, 0], matrix_diagonal, sample_size)
 
+# plot generated samples
+plt.scatter(samples_cm_identity[:, 0], samples_cm_identity[:, 1], color='teal',
+            label='Samples generated with Identity matrix')
+plt.scatter(samples_cm_diagonal[:, 0], samples_cm_diagonal[:, 1], color='pink',
+            label='Samples generated with Diagonal matrix')
